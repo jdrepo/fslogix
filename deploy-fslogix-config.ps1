91f5c821e6896e8f9fc218d98ad758af7667b95a
jdrepo <65734789+jdrepo@users.noreply.github.com>
2022-06-21 07:46:41 +0200

    Update deploy-fslogix-config.ps1

--- deploy-fslogix-config.ps1
+++ deploy-fslogix-config.ps1
@@ -38,6 +38,10 @@ try {
         New-ItemProperty -Path $fslogixPath -Name Enabled -Value 1 -PropertyType DWORD -Force | Out-Null
         New-ItemProperty -Path $fslogixPath -Name VHDLocations -Value $profileLocation -PropertyType String -Force | Out-Null
         New-ItemProperty -Path $fslogixPath -Name DeleteLocalProfileWhenVHDShouldApply -Value 1 -PropertyType DWORD -Force | Out-Null
+        New-ItemProperty -Path $fslogixPath -Name FlipFlopProfileDirectoryName -Value 1 -Property DWORD -Force | Out-Null
+        New-ItemProperty -Path $fslogixPath -Name IsDynamic -Value 1 -Property DWORD -Force | Out-Null
+        New-ItemProperty -Path $fslogixPath -Name ProfileType -Value 3 -Property DWORD -Force | Out-Null
+        New-ItemProperty -Path $fslogixPath -Name VolumeType -Value "VHDX" -PropertyType String -Force | Out-Null
         Write-Information "Configuring fslogix profile location"
     }
 }
